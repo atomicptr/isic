4c151e1f8ed971a48058691261a1aed6b2c4ce75
Christopher Kaster <atomicptr@gmail.com>
2017-01-19 20:53:03 +0100

    show error when config.json is not found

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const fs = require("fs")
+
+if(!fs.existsSync("./config.json")) {
+    console.error("ERR: No \"config.json\" found, please rename the config.template.json file to config.json and enter your Discord token there.")
+    process.exit(1)
+}
+
 const config = require("./config.json")
 const Bot = require("./src/Bot.js")
 
